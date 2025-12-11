18870cc319dfcc3899302438b8335f04da0be5b8
Yatagaru <nap1903@gmail.com>
2025-12-11 17:52:11 -0300

    Added Mega Drive Games + Responsiveness

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,6 +1,10 @@
 import "./Header.css"
+import { useState } from "react";
+
 export function Header({ onGameSearch }: { onGameSearch: (value: string) => void }) {
 
+     const [menuOpen, setMenuOpen] = useState(false);
+     
     return (
         <div className="header-container">
             <div className="header-logo-container">
@@ -19,6 +23,19 @@ export function Header({ onGameSearch }: { onGameSearch: (value: string) => void
             <div className="header-links">
                 <a href="./about">About</a>
                 <a href="./">Games</a>
+            </div>
+
+            <div 
+                className={`hamburger ${menuOpen ? "open" : ""}`}
+                onClick={() => setMenuOpen(!menuOpen)}
+            >
+                <span></span><span></span><span></span>
+            </div>
+
+            {/* MOBILE MENU */}
+            <div className={`mobile-menu ${menuOpen ? "show" : ""}`}>
+                <a href="/">Home</a>
+                <a href="/about">About</a>
             </div>
         </div>
     )
--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -13,82 +13,89 @@ export function HomePage() {
     const [displayedGames, setDisplayedGames] = useState<GameProps[]>([]); // State that will hold all the game cards which are on the screen
     const [exitingIds, setExitingIds] = useState<number[]>([]); // State that will hold the game card ids which will diplay the exit animation
     const [enteringIds, setEnteringIds] = useState<number[]>([]); // State that will hold the game card ids which will diplay the entrance animation
+    const [showFilters, setShowFilters] = useState<boolean>(); // State that will set the visibility of the filter button
 
     // Load games on mount
-   useEffect(() => {
-    fetch("/games.json")
-        .then(response => response.json())
-        .then((data: GameProps[]) => {
-            setAllGames(data);
-            setDisplayedGames(data);
+    useEffect(() => {
+        fetch("/games.json")
+            .then(response => response.json())
+            .then((data: GameProps[]) => {
+                setAllGames(data);
+                setDisplayedGames(data);
 
-            // Trigger entry animation for ALL cards on first load
-            setEnteringIds(data.map(g => g.id));
+                // Trigger entry animation for ALL cards on first load
+                setEnteringIds(data.map(g => g.id));
 
-            // Remove entering state after animation completes
-            setTimeout(() => {
-                setEnteringIds([]);
-            }, 600 + data.length * 4);
-        });
-}, []);
+                // Remove entering state after animation completes
+                setTimeout(() => {
+                    setEnteringIds([]);
+                }, 600 + data.length * 4);
+
+                if (window.innerWidth <= 768) {
+                    setShowFilters(false); // Mobile: hidden by default
+                } else {
+                    setShowFilters(true);  // Desktop: visible filters
+                }
+            });
+    }, []);
 
 
 
     // Apply filters with exit and entering animation
     const applyFiltersWithAnimation = (newFilters: string[], newSearch: string) => {
 
-    // What this basically does is collecting all games with the filters applied
-    const newVisibleGames = allGames.filter(game => {
-        const matchesConsole = newFilters.length === 0 || newFilters.includes(game.console);
-        const matchesSearch = newSearch === "" || game.title.toLowerCase().includes(newSearch.toLowerCase());
-        return matchesConsole && matchesSearch;
-    });
+        // What this basically does is collecting all games with the filters applied
+        const newVisibleGames = allGames.filter(game => {
+            const matchesConsole = newFilters.length === 0 || newFilters.includes(game.console);
+            const matchesSearch = newSearch === "" || game.title.toLowerCase().includes(newSearch.toLowerCase());
+            return matchesConsole && matchesSearch;
+        });
 
-    // Remove games that are already included in displayedGames
-    const removedGames = displayedGames.filter(
-        game => !newVisibleGames.some(g => g.id === game.id)
-    );
+        // Remove games that are already included in displayedGames
+        const removedGames = displayedGames.filter(
+            game => !newVisibleGames.some(g => g.id === game.id)
+        );
 
-    // Adding games to newVisibleGames
-    const addedGames = newVisibleGames.filter(
-        game => !displayedGames.some(g => g.id === game.id)
-    );
+        // Adding games to newVisibleGames
+        const addedGames = newVisibleGames.filter(
+            game => !displayedGames.some(g => g.id === game.id)
+        );
 
-    // If there is any removed game, do the exit animation
-    if (removedGames.length > 0) {
+        // If there is any removed game, do the exit animation
+        if (removedGames.length > 0) {
 
-        // Updating the state
-        setExitingIds(removedGames.map(g => g.id));
+            // Updating the state
+            setExitingIds(removedGames.map(g => g.id));
 
-        // Animation delay
-        setTimeout(() => {
+            // Animation delay
+            setTimeout(() => {
 
-            // Update the displayed games
+                // Update the displayed games
+                setDisplayedGames(newVisibleGames);
+
+                // Mark the new cards as entering
+                setEnteringIds(addedGames.map(g => g.id));
+
+                // Clean the exiting state
+                setExitingIds([]);
+
+                // Stop the entering animation after 500 ms
+                setTimeout(() => {
+                    setEnteringIds([]);
+                }, 600 + addedGames.length * 4);
+
+            }, 600 + addedGames.length * 4);
+        } else {
+            // If there are none removed, appear with the entrance animation
             setDisplayedGames(newVisibleGames);
 
-            // Mark the new cards as entering
             setEnteringIds(addedGames.map(g => g.id));
 
-            // Clean the exiting state
-            setExitingIds([]);
-
-            // Stop the entering animation after 500 ms
             setTimeout(() => {
                 setEnteringIds([]);
             }, 600 + addedGames.length * 4);
-
-        }, 600 + addedGames.length * 4);
-    } else {
-        // If there are none removed, appear with the entrance animation
-        setDisplayedGames(newVisibleGames);
-
-        setEnteringIds(addedGames.map(g => g.id));
-
-        setTimeout(() => {
-            setEnteringIds([]);
-        }, 600 + addedGames.length * 4);
-    }
-};
+        }
+    };
 
 
     // Handle console filter changes
@@ -96,7 +103,7 @@ export function HomePage() {
         const newFilters = isChecked
             ? [...consoleFilters, console]
             : consoleFilters.filter(c => c !== console);
-        
+
         // Updating the state of the filters
         setConsoleFilters(newFilters);
 
@@ -118,17 +125,28 @@ export function HomePage() {
     return (
         <div className="home-container">
             <Header onGameSearch={handleSearchBar} />
-            
-            <div className="filters-container">
-                <GameFilters 
-                    cardsArray={allGames} 
-                    onConsoleFilterChange={handleConsoleFilter}
-                />
-            </div>
+
+            {/* State to control the visibility of the button to show filters in the mobile */}
+            {showFilters && (
+                <div className="filters-container">
+                    <GameFilters
+                        cardsArray={allGames}
+                        onConsoleFilterChange={handleConsoleFilter}
+                    />
+                </div>
+            )}
+
+            {/* Button of filters (mobile) */}
+            <button
+                className="mobile-filter-button"
+                onClick={() => setShowFilters(prev => !prev)}
+            >
+                Filters
+            </button>
 
             <div className="card-grid">
                 <GameCardsGrid
-                    cardsArray={displayedGames.sort((a, b) => 
+                    cardsArray={displayedGames.sort((a, b) =>
                         a.console.localeCompare(b.console)
                     )}
                     onPlay={handlePlayROM}
